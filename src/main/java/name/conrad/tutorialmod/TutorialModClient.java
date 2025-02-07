935e895cbd0863cc1278ff7d56e4bc4d53837bed
Conrad Mutai <conradmutai@dyn172-30-24-206.wireless.uwo.pri>
2025-02-07 12:04:47 -0500

    Initial commit

---
+++ src/main/java/name/conrad/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package name.conrad.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
